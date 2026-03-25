637d9a20450e72865a22d80c3c4b6ee7adb62c6e
ScarrleTRain <felixmcwong+Github@gmail.com>
2026-03-26 11:38:00 +1300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TutorBridge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
