ad366e600ed00f3d8ea955421e8b89605b18ca41
ScarrleTRain <felixmcwong+Github@gmail.com>
2026-03-27 10:27:28 +1300

    Added Scaffolded items for classes

--- Areas/Identity/Data/TutorBridgeContext.cs
+++ Areas/Identity/Data/TutorBridgeContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TutorBridge.Areas.Identity.Data;
+using TutorBridge.Models;
 
 namespace TutorBridge.Areas.Identity.Data;
 
@@ -19,4 +20,12 @@ public class TutorBridgeContext : IdentityDbContext<User>
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
+public DbSet<TutorBridge.Models.Booking> Booking { get; set; } = default!;
+
+public DbSet<TutorBridge.Models.Subject> Subject { get; set; } = default!;
+
+public DbSet<TutorBridge.Models.Timeslot> Timeslot { get; set; } = default!;
+
+public DbSet<TutorBridge.Models.TutorSubject> TutorSubject { get; set; } = default!;
 }
---
+++ TutorSubjectsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TutorBridge.Areas.Identity.Data;
+using TutorBridge.Models;
+
+namespace TutorBridge
+{
+    public class TutorSubjectsController : Controller
+    {
+        private readonly TutorBridgeContext _context;
+
+        public TutorSubjectsController(TutorBridgeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TutorSubjects
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TutorSubject.ToListAsync());
+        }
+
+        // GET: TutorSubjects/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tutorSubject = await _context.TutorSubject
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tutorSubject == null)
+            {
+                return NotFound();
+            }
+
+            return View(tutorSubject);
+        }
+
+        // GET: TutorSubjects/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TutorSubjects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TutorId,SubjectId")] TutorSubject tutorSubject)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tutorSubject);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tutorSubject);
+        }
+
+        // GET: TutorSubjects/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tutorSubject = await _context.TutorSubject.FindAsync(id);
+            if (tutorSubject == null)
+            {
+                return NotFound();
+            }
+            return View(tutorSubject);
+        }
+
+        // POST: TutorSubjects/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TutorId,SubjectId")] TutorSubject tutorSubject)
+        {
+            if (id != tutorSubject.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tutorSubject);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TutorSubjectExists(tutorSubject.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tutorSubject);
+        }
+
+        // GET: TutorSubjects/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tutorSubject = await _context.TutorSubject
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tutorSubject == null)
+            {
+                return NotFound();
+            }
+
+            return View(tutorSubject);
+        }
+
+        // POST: TutorSubjects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tutorSubject = await _context.TutorSubject.FindAsync(id);
+            if (tutorSubject != null)
+            {
+                _context.TutorSubject.Remove(tutorSubject);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TutorSubjectExists(int id)
+        {
+            return _context.TutorSubject.Any(e => e.Id == id);
+        }
+    }
+}
