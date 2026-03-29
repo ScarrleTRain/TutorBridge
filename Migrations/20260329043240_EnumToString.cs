9c501af68405e79927e90fa93daa843c6cfa9c8f
ScarrleTRain <felixmcwong+Github@gmail.com>
2026-03-29 17:33:47 +1300

    Changed to saving Status as string rather than int

---
+++ Migrations/20260329043240_EnumToString.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutorBridge.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnumToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Subject",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Subject",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Booking",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Subject",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Subject",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Booking",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Models/Subject.cs
+++ Models/Subject.cs
@@ -8,12 +8,12 @@ namespace TutorBridge.Models
 
         // Makes sure there is a subject name, and that it is less than 100 chars
         [Required(ErrorMessage = "Subject name is required")] 
-        [StringLength(100, ErrorMessage = "Max 100 Characters"))]
+        [StringLength(100, ErrorMessage = "Max 100 Characters")]
         public required string Name { get; set; }
 
         // Makes sure there is a subject description, and that it is less than 500 chars
         [Required(ErrorMessage = "Subject description is required")]
-        [StringLength(500, ErrorMessage = "Max 500 Characters"))]
+        [StringLength(500, ErrorMessage = "Max 500 Characters")]
         public required string Description { get; set; }
     }
 }
