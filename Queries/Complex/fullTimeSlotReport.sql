3e5e626bd44b1f1c629599a04d06dea64e33708f
ScarrleTRain <felixmcwong+Github@gmail.com>
2026-04-23 09:30:12 +1200

    Little bug

--- Queries/Complex/fullTimeSlotReport.sql
+++ Queries/Complex/fullTimeSlotReport.sql
@@ -1,8 +1,8 @@
 SELECT t.TimeslotId AS Id,
        CONCAT(u.NameFirst, ' ', u.NameLast) AS [Tutor Name],
        CONVERT (VARCHAR, t.DateTimeStart, 103) AS [Date],
-       CONVERT (VARCHAR (5), CAST (t.DateTimeStart AS TIME)) AS StartTime,
-       CONVERT (VARCHAR (5), CAST (t.DateTimeEnd AS TIME)) AS StartTime
+       CONVERT (VARCHAR (5), CAST (t.DateTimeStart AS TIME)) AS [Start Time],
+       CONVERT (VARCHAR (5), CAST (t.DateTimeEnd AS TIME)) AS [End Time]
 FROM   Timeslot AS t
        LEFT OUTER JOIN
        AspNetUsers AS u
