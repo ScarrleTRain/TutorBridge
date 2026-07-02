982ad06afe3de376551a1b98d87542c7a69ab5b7
ScarrleTRain <felixmcwong+Github@gmail.com>
2026-07-02 20:26:08 +1200

    Added User Controller and Index page

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutorBridge.Areas.Identity.Data;
+using TutorBridge.ViewModels;
+using TutorBridge.Models;
+
+namespace TutorBridge.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var model = new List<UserVM>();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+
+                model.Add(new UserVM
+                {
+                    Id = user.Id,
+                    NameFirst = user.NameFirst,
+                    NameLast = user.NameLast,
+                    Email = user.Email ?? "",
+                    Phone = user.Phone,
+                    BirthDate = user.BirthDate,
+                    Blurb = user.Blurb,
+                    Role = roles.FirstOrDefault() ?? "No Role"
+                });
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/UserVM.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TutorBridge.Validation;
+
+namespace TutorBridge.ViewModels
+{
+    public class UserVM
+    {
+        public required string Id { get; set; }
+        public required string NameFirst { get; set; }
+        public required string NameLast { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string? Blurb { get; set; }
+        public required string Role { get; set; }
+    }
+}
