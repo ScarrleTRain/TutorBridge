e4507e76796e5a93364c34f33e6693103adc9c96
ScarrleTRain <felixmcwong+Github@gmail.com>
2026-07-19 15:24:36 +1200

    Update Booking pages

--- Controllers/BookingsController.cs
+++ Controllers/BookingsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -66,6 +67,10 @@ namespace TutorBridge.Controllers
             }
 
             var booking = await _context.Booking
+                .Include(b => b.User)
+                .Include(b => b.TimeSlot)
+                .Include(t => t.TimeSlot.Tutor)
+                .Include(b => b.Subject)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (booking == null)
             {
@@ -134,8 +139,12 @@ namespace TutorBridge.Controllers
         }
 
         // GET: Bookings/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            ViewBag.Users = await UserDropdown();
+            ViewBag.Timeslots = await TimeslotDropdown();
+            ViewBag.Subjects = await SubjectDropdown();
+
             return View();
         }
 
@@ -152,6 +161,11 @@ namespace TutorBridge.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
+            ViewBag.Users = await UserDropdown();
+            ViewBag.Timeslots = await TimeslotDropdown();
+            ViewBag.Subjects = await SubjectDropdown();
+
             return View(booking);
         }
 
@@ -168,6 +182,11 @@ namespace TutorBridge.Controllers
             {
                 return NotFound();
             }
+
+            ViewBag.Users = await UserDropdown();
+            ViewBag.Timeslots = await TimeslotDropdown();
+            ViewBag.Subjects = await SubjectDropdown();
+
             return View(booking);
         }
 
@@ -203,6 +222,11 @@ namespace TutorBridge.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
+
+            ViewBag.Users = await UserDropdown();
+            ViewBag.Timeslots = await TimeslotDropdown();
+            ViewBag.Subjects = await SubjectDropdown();
+
             return View(booking);
         }
 
@@ -215,11 +239,17 @@ namespace TutorBridge.Controllers
             }
 
             var booking = await _context.Booking
+                .Include(b => b.User)
+                .Include(b => b.TimeSlot)
+                .Include(t => t.TimeSlot.Tutor)
+                .Include(b => b.Subject)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (booking == null)
             {
                 return NotFound();
             }
+
+
 
             return View(booking);
         }
@@ -242,6 +272,44 @@ namespace TutorBridge.Controllers
         private bool BookingExists(int id)
         {
             return _context.Booking.Any(e => e.Id == id);
+        }
+
+        public async Task<IEnumerable<SelectListItem>> UserDropdown()
+        {
+            return (await _context.Users
+                .Select(u => new SelectListItem
+                {
+                    Value = u.Id,
+                    Text = $"{u.NameFirst} {u.NameLast}"
+                })
+                .ToListAsync())
+                .OrderBy(u => u.Text);
+        }
+
+        public async Task<IEnumerable<SelectListItem>> TimeslotDropdown()
+        {
+            return (await _context.TimeSlot
+                .Include(t => t.Tutor)
+                .Select(t => new SelectListItem
+                {
+                    Value = t.TimeSlotId.ToString(),
+                    Text = $"{t.Tutor.NameFirst} {t.Tutor.NameLast} // {t.DateTimeStart:d} {t.DateTimeStart:t} - {t.DateTimeEnd:t}"
+                })
+                .ToListAsync())
+                .OrderBy(t => t.Text);
+        }
+
+        public async Task<IEnumerable<SelectListItem>> SubjectDropdown()
+        {
+            return (await _context.Subject
+                .Select(s => new SelectListItem
+                {
+                    Value = s.SubjectId.ToString(),
+                    Text = $"{s.Name}"
+                })
+                .ToListAsync())
+                .OrderBy(s => s.Text);
+            ;
         }
     }
 }
